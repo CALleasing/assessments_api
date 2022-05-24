362eef1643c88ae733c2f5a13ee23c3da7b8421a
unknown <calleasing.kkn@gmail.com>
2022-05-24 15:03:42 +0700

    update API assessment

--- routes/answer.js
+++ routes/answer.js
@@ -1,29 +1,77 @@
-const express = require('express');
-const answersController = require('../controller/answer');
+const express = require("express");
+const answersController = require("../controller/answer");
 
 const router = express.Router();
 
-router.get('/staff/:year/:part/:userid', answersController.getStaffQuestionByUserId);
-router.get('/staff/:year/:part/:userid/:number', answersController.getStaffAnswerFromUserIdByNumber);
-router.post('/staff/:year/:part/:userid/:number', answersController.postOrPutStaffAnswer);
+router.get(
+  "/staff/:year/:part/:userid",
+  answersController.getStaffQuestionByUserId
+);
+router.get(
+  "/staff/:year/:part/:userid/:number",
+  answersController.getStaffAnswerFromUserIdByNumber
+);
+router.post(
+  "/staff/:year/:part/:userid/:number",
+  answersController.postOrPutStaffAnswer
+);
 // router.put('/staff/:year/:part/:userid/:number', answersController.putStaffAnswer);
 
-router.get('/manager/:year/:part/:userid', answersController.getManagerQuestionByUserId);
-router.get('/manager/:year/:part/:userid/:number', answersController.getManagerAnswerFromUserIdByNumber);
-router.post('/manager/:year/:part/:userid/:number', answersController.postOrPutManagerAnswer);
+router.get(
+  "/manager/:year/:part/:userid",
+  answersController.getManagerQuestionByUserId
+);
+router.get(
+  "/manager/:year/:part/:userid/:number",
+  answersController.getManagerAnswerFromUserIdByNumber
+);
+router.post(
+  "/manager/:year/:part/:userid/:number",
+  answersController.postOrPutManagerAnswer
+);
 // router.put('/manager/:year/:part/:userid/:number', answersController.putManagerAnswer);
 
-router.get('/comment/user/:year/:part/department/:department', answersController.getAllStaffComment);
-router.get('/comment/user/:year/:part/:manager_id/:userid', answersController.getStaffCommentByUserId);
-router.post('/comment/user/:year/:part/:userid/:number', answersController.postOrPutStaffComment);
+router.get(
+  "/comment/user/:year/:part/department/:department",
+  answersController.getAllStaffComment
+);
+router.get(
+  "/comment/user/:year/:part/:manager_id/:userid",
+  answersController.getStaffCommentByUserId
+);
+router.post(
+  "/comment/user/:year/:part/:userid/:number",
+  answersController.postOrPutStaffComment
+);
 
-router.get('/comment/manager/:year/:part/:manager_id/:number', answersController.getStaffCommentWithManagerIdAndNumber);
-router.get('/comment/manager/:year/:part/:manager_id', answersController.getAllStaffCommentWithManagerId);
+router.get(
+  "/comment/manager/:year/:part/:manager_id/:number",
+  answersController.getStaffCommentWithManagerIdAndNumber
+);
+router.get(
+  "/comment/manager/:year/:part/:manager_id",
+  answersController.getAllStaffCommentWithManagerId
+);
 
-router.get('/assessment', answersController.getAllEmployeeAssessment);
-router.get('/assessment/departments/:department', answersController.getEmployeeAssessmentByDepartment);
-router.get('/assessment/users/:userid', answersController.getEmployeeAssessmentByUserId);
-router.post('/assessment', answersController.postEmployeeAssessment);
-router.delete('/assessment', answersController.deleteEmployeeAssessmentFromUserid);
+router.get(
+  "/assessment/:year/:part",
+  answersController.getAllEmployeeAssessment
+);
+router.get(
+  "/assessment/:year/:part/department/:department",
+  answersController.getEmployeeAssessmentByDepartment
+);
+router.get(
+  "/assessment/:year/:part/users/:userid",
+  answersController.getEmployeeAssessmentByUserId
+);
+router.post(
+  "/assessment/:year/:part",
+  answersController.postEmployeeAssessment
+);
+router.delete(
+  "/assessment/:year/:part",
+  answersController.deleteEmployeeAssessmentFromUserid
+);
 
 module.exports = router;
