816d474628e075920ee2c9e1b032968049d6841a
unknown <calleasing.kkn@gmail.com>
2022-05-27 13:39:16 +0700

    fixed_node_modules

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const loginController = require('../controller/login');
-
-const router = express.Router();
-
-router.post('/', loginController.postLogin);
-
-module.exports = router;
